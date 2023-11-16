6f849fabcb571ab03a42a86a639afb9f8e04f8e2
EVissI <90708659+EVissI@users.noreply.github.com>
2023-11-16 11:24:19 +0300

    Delete CuponRequest directory

--- CuponRequest/src/main/java/com/example/CuponRequest/devtools/math/MathOperation.kt
+++
@@ -1,8 +0,0 @@
-package com.example.CuponRequest.devtools.math
-
-object MathOperation {
-    // Вычисление процентного изменения
-    fun calculatePercentageChange(oldValue: Double, newValue: Double): Int {
-        return Math.toIntExact(Math.round(Math.abs((newValue - oldValue) / oldValue * 100)))
-    }
-}
